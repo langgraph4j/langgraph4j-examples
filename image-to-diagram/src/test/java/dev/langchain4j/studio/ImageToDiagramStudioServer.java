a4db1fa008eb97287259d55470fc272924caaea9
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-05-20 13:09:25 +0200

    feat: add langgraph4j studio support

---
+++ image-to-diagram/src/test/java/dev/langchain4j/studio/ImageToDiagramStudioServer.java
@@ -0,0 +1,40 @@
+package dev.langchain4j.studio;
+
+import dev.langchain4j.DotEnvConfig;
+import dev.langchain4j.image_to_diagram.ImageToDiagramProcess;
+import org.bsc.langgraph4j.GraphRepresentation;
+import org.bsc.langgraph4j.studio.jetty.LangGraphStreamingServerJetty;
+
+public class ImageToDiagramStudioServer {
+
+
+    public static void main(String[] args) throws Exception {
+
+        DotEnvConfig.load();
+
+        // var openApiKey = DotEnvConfig.valueOf("OPENAI_API_KEY")
+        //        .orElseThrow( () -> new IllegalArgumentException("no OPENAI API KEY provided!"));
+
+        // var imageData = ImageLoader.loadImageAsBase64( "LangChainAgents.png" );
+
+        var agentExecutor = new ImageToDiagramProcess();
+
+        var workflow = agentExecutor.workflowWithCorrection();
+
+        System.out.println (
+                workflow.getGraph(GraphRepresentation.Type.MERMAID, "Image To Diagram", false)
+                        .content()
+        );
+
+        var server = LangGraphStreamingServerJetty.builder()
+                .port(8080)
+                .title("Image To Diagram")
+                .addInputImageArg( "imageData" )
+                .stateGraph(workflow)
+                .build();
+
+        server.start().join();
+
+    }
+
+}
