8691b894f260c645644f95b108b70cf720d4cd06
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-07-07 10:53:14 +0200

    remove mcp

---
+++ spring-ai/mcp-client-agent/PROMPT.md
@@ -0,0 +1,8 @@
+
+execute test
+
+execute test twice
+
+get current threads of system
+
+get the current thread count and pass result to new test 
---
+++ spring-ai/mcp-client-agent/src/main/java/org/bsc/langgraph4j/mcp/TestTool.java
@@ -0,0 +1,23 @@
+package org.bsc.langgraph4j.mcp;
+
+
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+
+import static java.lang.String.format;
+
+public class TestTool {
+
+    @Tool( description = "tool for test AI agent executor")
+    String execTest( @ToolParam( description = "test message") String message) {
+        return format( "test tool ('%s') executed with result 'OK'", message);
+    }
+
+    @Tool( description = "return current number of system thread allocated by application")
+    int threadCount() {
+        return Thread.getAllStackTraces().size();
+    }
+
+
+
+}
