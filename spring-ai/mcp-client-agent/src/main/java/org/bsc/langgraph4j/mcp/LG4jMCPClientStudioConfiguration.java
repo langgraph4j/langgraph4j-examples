116502bd1d38e1ac047c156268214cb00cccce7c
bsorrentino <bartolomeo.sorrentino@gmail.com>
2025-12-02 22:18:53 +0100

    refactor(spring-ai/mcp-client-agent): Refactor configuration to use new studio config base class
    Update class to extend LangGraphStudioConfig instead of AbstractLangGraphStudioConfig. Add logging and implement instanceMap() method for studio server configuration.

--- spring-ai/mcp-client-agent/src/main/java/org/bsc/langgraph4j/mcp/LG4jMCPClientStudioConfiguration.java
+++ spring-ai/mcp-client-agent/src/main/java/org/bsc/langgraph4j/mcp/LG4jMCPClientStudioConfiguration.java
@@ -5,39 +5,48 @@ import org.bsc.langgraph4j.GraphStateException;
 import org.bsc.langgraph4j.StateGraph;
 import org.bsc.langgraph4j.checkpoint.MemorySaver;
 import org.bsc.langgraph4j.state.AgentState;
-import org.bsc.langgraph4j.studio.springboot.AbstractLangGraphStudioConfig;
+import org.bsc.langgraph4j.studio.LangGraphStudioServer;
 import org.bsc.langgraph4j.studio.springboot.LangGraphFlow;
+import org.bsc.langgraph4j.studio.springboot.LangGraphStudioConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.chat.messages.UserMessage;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Map;
 import java.util.Objects;
 
 @Configuration
-public class LG4jMCPClientStudioConfiguration extends AbstractLangGraphStudioConfig {
+public class LG4jMCPClientStudioConfiguration extends LangGraphStudioConfig {
+    private Logger log = LoggerFactory.getLogger(LG4jMCPClientStudioConfiguration.class);
 
-    private final LangGraphFlow flow;
+    private final LG4jMCPClientService agentService;
 
-    public LG4jMCPClientStudioConfiguration( LG4jMCPClientService agentService ) throws GraphStateException {
-
-        this.flow = agentWorkflow( agentService.getAgentWorkflow() );
-    }
-
-    private LangGraphFlow agentWorkflow( StateGraph<? extends AgentState> workflow ) throws GraphStateException {
-
-        return  LangGraphFlow.builder()
-                .title("LangGraph4j MCP Client")
-                .addInputStringArg( "messages", true, v -> new UserMessage( Objects.toString(v) ) )
-                .stateGraph( workflow )
-                .compileConfig( CompileConfig.builder()
-                        .checkpointSaver( new MemorySaver() )
-                        .releaseThread(true)
-                        .build())
-                .build();
-
+    public LG4jMCPClientStudioConfiguration( LG4jMCPClientService agentService ) {
+        this.agentService = agentService;
     }
 
     @Override
-    public LangGraphFlow getFlow() {
-        return this.flow;
+    public Map<String, LangGraphStudioServer.Instance> instanceMap() {
+        try {
+
+            var instance =  LangGraphStudioServer.Instance.builder()
+                    .title("LangGraph4j MCP Client")
+                    .addInputStringArg( "messages", true, v -> new UserMessage( Objects.toString(v) ) )
+                    .graph( agentService.getAgentWorkflow() )
+                    .compileConfig( CompileConfig.builder()
+                            .checkpointSaver( new MemorySaver() )
+                            .releaseThread(true)
+                            .build())
+                    .build();
+
+            return Map.of("agent", instance);
+
+        } catch (GraphStateException e) {
+            log.error(e.getMessage(), e);
+            return Map.of();
+        }
+
+
     }
 }
